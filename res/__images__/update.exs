8c160790f01caa360062de3260a60e06ef985cb6
camshaft <bytheway.cameron@gmail.com>
2016-02-01 12:41:25 -0700

    add forward_to support

---
+++ res/__images__/update.exs
@@ -0,0 +1,32 @@
+@doc """
+Update a user
+"""
+
+@action :replace
+
+service = Resource.param(:service)
+function = Resource.param(:function) || :update_image
+model = Resource.param(:model)
+
+{after_update, params} = case Resource.param(:after) do
+  redirect when is_tuple(redirect) ->
+    redirect
+  redirect when is_atom(redirect) ->
+    {redirect, []}
+end
+
+action do
+  apply(service, function, Input.get())
+  transition_to(after_update, params)
+end
+
+affordance do
+  %{
+    "input" => %{
+      "image" => %{
+        "type" => "url",
+        "value" => model.image
+      }
+    }
+  }
+end
---
+++ res/users/@user/update_image.exs
@@ -0,0 +1,12 @@
+@doc """
+Update a user
+"""
+
+param user, do: User.get(&1)
+
+forward_to("../../__images__/update", user: user, %{
+  service: User,
+  function: :update_image,
+  model: user,
+  after: {resolve("./"), user: user}
+})
